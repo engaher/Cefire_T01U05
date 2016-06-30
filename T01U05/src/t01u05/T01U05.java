36ebd465019599b614741a77321632093ea7b1fe
Enrique Garcia <enrique.garciah@gmail.com>
2016-06-30 12:20:44 +0200

    Leer Personas

--- T01U05/src/t01u05/T01U05.java
+++ T01U05/src/t01u05/T01U05.java
@@ -15,6 +15,18 @@ public class T01U05 {
         Persona [] personas = new Persona[3];
         Persona p;
         String nombre, nif;
+        //// Leer Personas
+        for(int i = 0; i < personas.length; i++){
+            System.out.println("Persona " + (i+1));
+            System.out.print("Nif: ");
+            nif = sc.nextLine();
+            System.out.print("Nombre: ");
+            nombre = sc.nextLine();
+            p = new Persona();
+            p.setNif(nif);
+            p.setNombre(nombre);
+            personas[i]= p;
+        }
     }
     
 }
